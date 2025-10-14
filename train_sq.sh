4ffd2ab7f1e2d4d82f5776f3ad826a182e76f1ed
junseo0421 <jmdmk9st@naver.com>
2025-10-14 15:54:36 +0900

    sdfg

--- train_sq.sh
+++ train_sq.sh
@@ -2,5 +2,5 @@ CUDA_VISIBLE_DEVICES=0 torchrun --nproc_per_node=1 --master_port=29505 debug.py 
  --epochs 500 \
  --lr 5.e-4 \
  --scale_range [0.75,1.25] \
- --crop_size [1024,1024] \
+ --crop_size [1024,1920] \
  --batch_size 8 \
