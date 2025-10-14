7b7d8207f1b44a5131ed3aba527b60ab9d41b73f
junseo0421 <jmdmk9st@naver.com>
2025-10-14 15:55:44 +0900

    fasd

--- train_sq.sh
+++ train_sq.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0 torchrun --nproc_per_node=1 --master_port=29505 debug.py \
+CUDA_VISIBLE_DEVICES=0 torchrun --nproc_per_node=1 --master_port=29505 train_pidnet.py \
  --epochs 500 \
  --lr 5.e-4 \
  --scale_range [0.75,1.25] \
